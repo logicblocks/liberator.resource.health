fc9c14c2c79fd2d38b183033ba504fd092796bc5
Toby Clemson <tobyclemson@gmail.com>
2020-06-18 17:49:58 +0100

    Add skeletal handler.

--- src/liberator_hal/health_resource/core.clj
+++ src/liberator_hal/health_resource/core.clj
@@ -1,1 +1,26 @@
-(ns liberator-hal.health-resource.core)
+(ns liberator-hal.health-resource.core
+  (:require
+    [liberator-mixin.core :as mixin]
+    [liberator-mixin.json.core :as json-mixin]
+    [liberator-mixin.hypermedia.core :as hypermedia-mixin]
+    [liberator-mixin.hal.core :as hal-mixin]
+    [halboy.resource :as hal]
+    [hype.core :as hype]))
+
+(defn definitions
+  ([dependencies] (definitions dependencies {}))
+  ([{:keys [routes]}
+    _]
+   {:handle-ok
+    (fn [{:keys [request]}]
+      (hal/new-resource
+        (hype/absolute-url-for request routes :health)))}))
+
+(defn handler
+  ([dependencies] (handler dependencies {}))
+  ([dependencies options]
+   (mixin/build-resource
+     (json-mixin/with-json-mixin dependencies)
+     (hypermedia-mixin/with-hypermedia-mixin dependencies)
+     (hal-mixin/with-hal-mixin dependencies)
+     (definitions dependencies options))))
--- test/liberator_hal/health_resource/core_test.clj
+++ test/liberator_hal/health_resource/core_test.clj
@@ -1,6 +1,45 @@
 (ns liberator-hal.health-resource.core-test
   (:require
-   [clojure.test :refer :all]))
+    [clojure.test :refer :all]
 
-(deftest example
-  (is (= 1 1)))
+    [halboy.resource :as hal]
+    [halboy.json :as hal-json]
+
+    [ring.mock.request :as ring]
+    [ring.middleware.keyword-params :as ring-keyword-params]
+    [ring.middleware.params :as ring-params]
+
+    [liberator-hal.health-resource.core :as health-resource]))
+
+(def health-route ["/health" :health])
+
+(defn routes [extras]
+  [""
+   (concat
+     [health-route]
+     extras)])
+
+(defn dependencies
+  ([] (dependencies []))
+  ([extra-routes]
+   {:routes (routes extra-routes)}))
+
+(defn resource-handler
+  ([dependencies] (resource-handler dependencies {}))
+  ([dependencies options]
+   (-> (health-resource/handler dependencies options)
+     ring-keyword-params/wrap-keyword-params
+     ring-params/wrap-params)))
+
+(deftest has-status-200
+  (let [handler (resource-handler (dependencies))
+        request (ring/request :get "/")
+        result (handler request)]
+    (is (= (:status result) 200))))
+
+(deftest includes-self-link
+  (let [handler (resource-handler (dependencies))
+        request (ring/request :get "http://localhost/health")
+        result (handler request)
+        resource (hal-json/json->resource (:body result))]
+    (is (= (hal/get-href resource :self) "http://localhost/health"))))
